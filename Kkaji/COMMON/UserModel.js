5fa68bbd6650a7ef123003f3ef98543309af2c89
Devmas <toctodavid@gmail.com>
2015-11-09 23:48:45 +0900

    Add UserModel
    -

---
+++ Kkaji/COMMON/UserModel.js
@@ -0,0 +1,174 @@
+Kkaji.UserModel = OBJECT({
+
+	preset : function() {
+		'use strict';
+
+		return Kkaji.MODEL;
+	},
+
+	params : function() {
+		'use strict';
+
+		var
+		
+		// create valid data set
+		createValidDataSet = {
+
+			name : {
+				notEmpty : true,
+				size : {
+					max : 255
+				}
+			},
+
+			email : {
+				notEmpty : true,
+				size : {
+					min : 5,
+					max : 320
+				},
+				email : true
+			},
+
+			password : {
+				notEmpty : true,
+				size : {
+					min : 4,
+					max : 20
+				}
+			},
+
+			birthYear : {
+				notEmpty : true,
+				integer : true
+			},
+
+			birthMonth : {
+				notEmpty : true,
+				integer : true
+			},
+
+			birthDate : {
+				notEmpty : true,
+				integer : true
+			},
+
+			phoneNumber : {
+				notEmpty : true,
+				size : {
+					min : 10,
+					max : 11
+				}
+			},
+
+			gender : {
+				notEmpty : true,
+				one : ['male', 'female']
+			},
+			
+			countryCode : {
+				notEmpty : true,
+				size : {
+					min : 2,
+					max : 2
+				}
+			},
+
+			loginCount : {
+				notEmpty : true,
+				integer : true
+			},
+
+			lastLoginTime : {
+				date : true
+			},
+
+			android : {
+				size : {
+					max : 255
+				}
+			},
+
+			ios : {
+				size : {
+					max : 255
+				}
+			},
+			
+			isPushOff : {
+				bool : true
+			},
+			
+			lockPassword : {
+				size : 4
+			},
+			
+			shareCount : {
+				notEmpty : true,
+				integer : true
+			}
+		},
+
+		// update valid data set
+		updateValidDataSet = COMBINE([createValidDataSet, {
+			newPassword : {
+				size : {
+					min : 4,
+					max : 20
+				}
+			}
+		}]);
+
+		return {
+			name : 'User',
+			initData : {
+				loginCount : 0,
+				shareCount : 0
+			},
+			methodConfig : {
+				create : {
+					valid : VALID(createValidDataSet)
+				},
+				update : {
+					valid : VALID(updateValidDataSet),
+					authKey : 'id',
+					adminRole : BeautyCareServer.ROLE.HOSPITAL
+				},
+				remove : {
+					role : BeautyCareServer.ROLE.SUPER_MANAGER
+				}
+			},
+			loginValid : VALID({
+
+				email : {
+					notEmpty : true,
+					size : {
+						min : 5,
+						max : 320
+					},
+					email : true
+				},
+
+				password : {
+					notEmpty : true,
+					size : {
+						min : 4,
+						max : 20
+					}
+				},
+
+				android : {
+					size : {
+						max : 255
+					}
+				},
+
+				ios : {
+					size : {
+						max : 255
+					}
+				}
+			})
+		};
+	}
+});
